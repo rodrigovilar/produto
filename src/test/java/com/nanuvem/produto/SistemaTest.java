aea9e591c6e79635f328eb5c576b9043abc6412c
rodrigovilar <rvilar@gmail.com>
2014-08-09 12:56:29 -0300

    Cadastrar dois produtos simples e emitir relatório de estoque

--- src/test/java/com/nanuvem/produto/SistemaTest.java
+++ src/test/java/com/nanuvem/produto/SistemaTest.java
@@ -13,8 +13,21 @@ public class SistemaTest {
 		Sistema sistema = new Sistema();
 		sistema.cadastrarProdutoSimples("Trigo", 15.33, "kg", 500);
 		
-		String relatorioEstoqueEsperado = "Trigo|15.33|kg|500.000";
+		String relatorioEstoqueEsperado = "Trigo|15.33|kg|500.000;";
 		
 		Assert.assertEquals(relatorioEstoqueEsperado, sistema.relatorioEstoque());
 	}
+	
+	@Test
+	public void cadastroProdutosSimples() {
+		Sistema sistema = new Sistema();
+		sistema.cadastrarProdutoSimples("Trigo", 15.33, "kg", 500);
+		sistema.cadastrarProdutoSimples("Laranja", 3.05, "kg", 100);
+		
+		String relatorioEstoqueEsperado = "Trigo|15.33|kg|500.000;"
+				+ "Laranja|3.05|kg|100.000;";
+		
+		Assert.assertEquals(relatorioEstoqueEsperado, sistema.relatorioEstoque());
+	}
+
 }
